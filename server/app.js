b4f7414a9ca1f982f8307a8f5c2bf09841954873
lootster <sheldonkhlim@gmail.com>
2018-11-05 15:34:07 +0800

    remove the hardecoded mongodb URL from server.js

--- server/app.js
+++ server/app.js
@@ -8,7 +8,21 @@ const path = require("path");
 const isMongooseConnectionProvided = process.env.NODE_ENV === "integration";
 
 if (!isMongooseConnectionProvided) {
-  mongoose.connect(process.env.MONGODB_URI);
+  mongoose.connect(process.env.MONGODB_URI, {
+    useNewUrlParser: true
+  });
+  mongoose.set('useCreateIndex', true);
+
+  const db = mongoose.connection;
+
+  db.once("open", () => {
+    console.log("Database is connected");
+  });
+
+  db.on("error", error => {
+    console.error("An error has occur", error);
+  });
+
 }
 
 const staticFiles = express.static(path.join(__dirname, "../client/build"));
--- server/server.js
+++ server/server.js
@@ -1,20 +1,4 @@
-const mongoose = require('mongoose');
 const app = require('./app');
-
-mongoose.connect("mongodb://localhost/first-backend-project", {
-  useNewUrlParser: true
-});
-mongoose.set('useCreateIndex', true);
-
-const db = mongoose.connection;
-
-db.once("open", () => {
-  console.log("Database is connected");
-});
-
-db.on("error", error => {
-  console.error("An error has occur", error);
-});
 
 app.listen(3001, () => {
   console.log("Listening on port 3001 ...")
